e9bf63c666df7b54394f5416bc4d36d432d31445
Dominik Živko <domy.zivko@gmail.com>
2022-04-11 23:17:37 +0200

    Make site skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import BookshelfPage from "./pages/BookshelfPage";
+import HomePage from "./pages/HomePage"
+import WishlistPage from "./pages/WishlistPage";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navigation />
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/bookshelf" element={<BookshelfPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+        </Routes>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
-
-export default App;
---
+++ client/src/pages/BookshelfPage.js
@@ -0,0 +1,6 @@
+
+export default function BookshelfPage() {
+    return (
+        "Bookshelf page"
+    );
+}
