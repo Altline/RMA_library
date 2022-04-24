741bd4374501a71b638d6f8474192ca8a0214164
Dominik Živko <domy.zivko@gmail.com>
2022-04-24 13:34:47 +0200

    Correct the registration page

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -1,31 +1,37 @@
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button";
-import { Row, Col } from "react-bootstrap"
-import "./RegistrationPage.css"
+import { Row, Col, Container } from "react-bootstrap"
 
 export default function RegistrationPage() {
     return (
-        <Row>
-            <Col lg={{ span: 4, offset: 4 }}>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="email" placeholder="Enter username" />
-                    </Form.Group>
+        <Container>
+            <Row className="text-align-start">
+                <Col lg={{ span: 4, offset: 4 }}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="email" placeholder="Choose a username" />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Choose a password" />
+                        </Form.Group>
 
-                    <Form.Group lg={4} className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check className="checkbox" type="checkbox" label="Remember me" />
-                    </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPasswordConfirm">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Repeat the password" />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
                     <Button variant="primary" type="submit">
-                        Submit
+                        Register
                     </Button>
-                </Form>
-            </Col>
-        </Row>
+                </Col>
+            </Row>
+        </Container>
     );
 }
