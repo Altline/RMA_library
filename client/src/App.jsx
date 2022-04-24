c22b3fe2815ac40c341f317fcf2cf4d76eb7afcb
Dominik Živko <domy.zivko@gmail.com>
2022-04-24 13:35:13 +0200

    Add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import BookshelfPage from "./pages/BookshelfPage";
 import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import WishlistPage from "./pages/WishlistPage";
 
@@ -20,6 +21,7 @@ export default function App() {
             <Route path="/bookshelf" element={<BookshelfPage />} />
             <Route path="/wishlist" element={<WishlistPage />} />
             <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         </Container>
       </main>
