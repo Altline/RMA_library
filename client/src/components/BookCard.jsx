662fe30df92c239c55adfcb4c521a2157b0c60a7
Dominik Živko <domy.zivko@gmail.com>
2022-04-27 18:07:12 +0200

    Add BookCard and BookCardList components

---
+++ client/src/components/BookCard.jsx
@@ -0,0 +1,32 @@
+import { Card, Col, Container, Row } from "react-bootstrap"
+import ReactHtmlParser from "react-html-parser"
+import { clipString } from "../Utils";
+
+
+export default function BookCard(props) {
+    const book = props.book;
+
+    return (
+        <Card className="book-card">
+            <Container>
+                <Row xs={1} md={2} className="justify-content-center justify-content-md-start">
+                    <Col xs="auto" md="auto" style={{ alignSelf: "center" }}>
+                        <Card.Img src={book.imageLinks.thumbnail} />
+                    </Col>
+                    <Col md={9} xl={10}>
+                        <Card.Body>
+                            <Card.Title>{book.title}</Card.Title>
+                            <Card.Subtitle>
+                                {book.subtitle + " - "}
+                                <i>{book.authors.join(", ")}</i>
+                            </Card.Subtitle>
+                            <Card.Text>
+                                {ReactHtmlParser(clipString(book.description, 600))}
+                            </Card.Text>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Container>
+        </Card>
+    );
+}
---
+++ client/src/components/BookCardList.jsx
@@ -0,0 +1,15 @@
+import BookCard from "./BookCard";
+
+export default function BookCardList(props) {
+    const books = props.books;
+
+    const cards = books.map((element) => 
+        <BookCard book={element} />
+    );
+    
+    return (
+        <div>
+            {cards}
+        </div>
+    );
+}
