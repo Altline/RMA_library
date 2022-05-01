039b66be4c026885903814d8df3bbbfd11afd4c1
Dominik Živko <domy.zivko@gmail.com>
2022-05-01 16:39:38 +0200

    Add sample book data to show on home page

--- client/src/components/BookCardList.jsx
+++ client/src/components/BookCardList.jsx
@@ -3,7 +3,7 @@ import BookCard from "./BookCard";
 export default function BookCardList(props) {
     const books = props.books;
 
-    const cards = books.map((element) => 
+    const cards = books?.map((element) => 
         <BookCard book={element} />
     );
     
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
+import React, { useEffect, useState } from "react";
+import BookApi from "../BookApi";
+import BookCardList from "../components/BookCardList";
 
 export default function HomePage() {
+    const [books, setBooks] = useState(null)
+
+    useEffect(() => {
+      BookApi.queryBooks("").then((res) => setBooks(res.items));
+    }, []);
+
     return (
-        "Home page"
+        <BookCardList books={books}/>
     );
 }
