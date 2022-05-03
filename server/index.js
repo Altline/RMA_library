45653f8bd685eef48fcf45411af3a7740580e528
Dominik Zivko <domy.zivko@gmail.com>
2022-05-03 12:18:14 +0200

    Adapt search endpoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const PORT = process.env.PORT || 3001;
 const searchRouter = require("./routes/search")
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/search",searchRouter)
 
 app.listen(PORT, () => {
--- server/routes/search.js
+++ server/routes/search.js
@@ -2,33 +2,44 @@ const express = require("express")
 const axios = require("axios")
 const router = express.Router()
 
-router.get("",getBooks)
+router.get("", getBooks)
 
-async function getBooks(req,res,next){
+async function getBooks(req, res, next) {
+    const q = req.query.q;
+    const maxResults = req.query.maxResults;
+
     var rawData;
-    const q=req.query.q;
-    const maxResults=req.query.maxResults;
-    var target=`https://www.googleapis.com/books/v1/volumes?q=${q}`;
-    if(maxResults!=null){
-        target=target.concat(`&maxResults=${maxResults}`);
-    }
-    await axios.get(target)
-    .then(function(response){rawData=response.data})
-    .catch(function (error) {console.log(error);})
+    var target = `https://www.googleapis.com/books/v1/volumes?q=${q}`;
 
-    var data = {totalItems: rawData.totalItems,
-    items: rawData.items.map(({id,selfLink,volumeInfo:{title,subtitle,authors,publisher,publishedDate,description,categories,imageLinks}})=>{return {
-        id: id,
-        selfLink: selfLink,
-        title: title,
-        subtitle:subtitle,
-        authors:authors,
-        publisher:publisher,
-        publishedDate:publishedDate,
-        description:description,
-        categories:categories,
-        imageLinks:imageLinks};
-    })
+    if (maxResults != null) {
+        target = target.concat(`&maxResults=${maxResults}`);
+    }
+
+    await axios.get(target)
+        .then(function (response) { rawData = response.data })
+        .catch(function (error) { console.log(error); })
+
+    const data = {
+        totalItems: rawData.totalItems,
+        items: rawData.items.map(
+            ({
+                id, selfLink, volumeInfo: {
+                    title, subtitle, authors, publisher, publishedDate, description, categories, imageLinks
+                }
+            }) => {
+                return {
+                    id: id ? id : "",
+                    selfLink: selfLink ? selfLink : "",
+                    title: title ? title : "",
+                    subtitle: subtitle ? subtitle : "",
+                    authors: authors ? authors : [],
+                    publisher: publisher ? publisher : "",
+                    publishedDate: publishedDate ? publishedDate : "",
+                    description: description ? description : "",
+                    categories: categories ? categories : [],
+                    imageLinks: imageLinks ? imageLinks : null
+                };
+            })
     };
 
     res.json(data);
