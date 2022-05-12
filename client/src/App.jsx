7b0468e811c3e341bc2053f006e59edc3d3c06f4
michael <michael.srempf@gmail.com>
2022-05-12 15:54:26 +0200

    Login/LogOut/Register functions implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,10 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import BookApi from "./BookApi";
 import Navigation from "./components/Navigation";
+import { AuthProvider } from "./contexts/authContext";
 import BookPage from "./pages/BookPage";
 import BookshelfPage from "./pages/BookshelfPage";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import WishlistPage from "./pages/WishlistPage";
@@ -19,26 +20,28 @@ export default function App() {
 
   return (
     <div className="App">
-      <header>
-        <Navigation onSearch={onSearch} />
-      </header>
-      <main>
-        <Container className="my-4">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search/:searchQuery" element={<HomePage />} />
-            <Route path="/bookshelf" element={<BookshelfPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-            <Route path="/book/:bookId" element={<BookPage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </Container>
-      </main>
-      <footer>
-        <p>Web programiranje, FERIT Osijek</p>
-        <p>Michael Šrempf, Dominik Živko</p>
-      </footer>
+      <AuthProvider>
+        <header>
+          <Navigation onSearch={onSearch} />
+        </header>
+        <main>
+          <Container className="my-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/search/:searchQuery" element={<HomePage />} />
+              <Route path="/bookshelf" element={<BookshelfPage />} />
+              <Route path="/wishlist" element={<WishlistPage />} />
+              <Route path="/book/:bookId" element={<BookPage />} />
+              <Route path="/register" element={<RegistrationPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <p>Web programiranje, FERIT Osijek</p>
+          <p>Michael Šrempf, Dominik Živko</p>
+        </footer>
+      </AuthProvider>
     </div>
   );
 }
