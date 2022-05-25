80f9c120ccf71a1046b83ff68cbb2daf0e35dd7c
Dominik Živko <domy.zivko@gmail.com>
2022-05-25 21:55:05 +0200

    Add bookshelf and wishlist pages

--- client/src/firebase/firebasedb.js
+++ client/src/firebase/firebasedb.js
@@ -1,4 +1,4 @@
-import { getFirestore, collection, setDoc, doc, getDoc } from "firebase/firestore"
+import { getFirestore, collection, setDoc, doc, getDoc, getDocs } from "firebase/firestore"
 import app from "./firebase"
 
 const db = getFirestore(app)
@@ -8,8 +8,31 @@ var booksRef;
 export function setUserDBref(userID) {
     booksRef = collection(db, "users", userID, "books");
 }
-export function clearuserDBref() { 
+export function clearuserDBref() {
     booksRef = null;
+}
+
+export async function getBook(bookId) {
+    if (booksRef) {
+        const documentRef = doc(booksRef, bookId)
+        const snapshot = await getDoc(documentRef)
+        if (snapshot.exists()) {
+            return snapshot.data();
+        }
+    }
+}
+
+export async function getBookShelf() {
+    if (booksRef) {
+        var bookShelfBooks = [];
+        const books = await getDocs(booksRef);
+        books.forEach(e => {
+            if (e.data().bookShelf) {
+                bookShelfBooks = bookShelfBooks.concat(e);
+            }
+        });
+        return bookShelfBooks;
+    }
 }
 
 export async function setOnBookShelfDB(bookId, status) {
@@ -21,22 +44,25 @@ export async function setOnBookShelfDB(bookId, status) {
     }
 }
 
+export async function getWishlist() {
+    if (booksRef) {
+        var wishlistBooks = [];
+        const books = await getDocs(booksRef);
+        books.forEach(e => {
+            if (e.data().wishlist) {
+                wishlistBooks = wishlistBooks.concat(e);
+            }
+        });
+        return wishlistBooks;
+    }
+}
+
 export async function setOnWishlistDB(bookId, status) {
     if (booksRef) {
         const documentRef = doc(booksRef, bookId);
         await setDoc(documentRef, {
             wishlist: status
         }, { merge: true })
-    }
-}
-
-export async function getBook(bookId) {
-    if (booksRef) {
-        const documentRef = doc(booksRef, bookId)
-        const snapshot = await getDoc(documentRef)
-        if (snapshot.exists()) {
-            return snapshot.data();
-        }
     }
 }
 
