75d12b74ea79a9881b8bc9135662f07b71d9a34a
Dominik Živko <domy.zivko@gmail.com>
2022-05-25 20:24:53 +0200

    Make book controls unavailable while not logged in

--- client/src/firebase/firebasedb.js
+++ client/src/firebase/firebasedb.js
@@ -1,53 +1,43 @@
-import { async } from "@firebase/util";
-import {getFirestore, collection, setDoc, doc, getDoc} from "firebase/firestore"
+import { getFirestore, collection, setDoc, doc, getDoc } from "firebase/firestore"
 import app from "./firebase"
 
-
 const db = getFirestore(app)
-var userRef;
 
+var booksRef;
 
-export function setUserDBref(userID){
-    userRef = collection(db,"users", userID,"books");
+export function setUserDBref(userID) {
+    booksRef = collection(db, "users", userID, "books");
 }
-export function clearuserDBref(){userRef=null;}
+export function clearuserDBref() { 
+    booksRef = null;
+}
 
-export async function setOnBookShelfDB(bookId, status){
-    console.log("outside")
-    if(userRef!==null){
-        console.log("inside")
-        const documentRef = doc(userRef,bookId);
-        await setDoc(documentRef,{
-        bookShelf: status
-    }, {merge: true})
+export async function setOnBookShelfDB(bookId, status) {
+    if (booksRef) {
+        const documentRef = doc(booksRef, bookId);
+        await setDoc(documentRef, {
+            bookShelf: status
+        }, { merge: true })
     }
 }
 
-export async function setOnWishlistDB(bookId, status){
-    console.log("outside 2")
-    if(userRef!==null){  
-        console.log("inside 2")  
-        const documentRef = doc(userRef,bookId);
-        await  setDoc(documentRef,{
-        wishlist: status
-    }, {merge: true})
+export async function setOnWishlistDB(bookId, status) {
+    if (booksRef) {
+        const documentRef = doc(booksRef, bookId);
+        await setDoc(documentRef, {
+            wishlist: status
+        }, { merge: true })
     }
 }
 
-
-export async function getBook(bookId){
-    if(userRef!==null){
-        const documentRef = doc(userRef,bookId)
+export async function getBook(bookId) {
+    if (booksRef) {
+        const documentRef = doc(booksRef, bookId)
         const snapshot = await getDoc(documentRef)
-        if(snapshot.exists()){
-           return snapshot.data();
-        }else{
-            console.log("no data")
+        if (snapshot.exists()) {
+            return snapshot.data();
         }
     }
 }
-
-
-
 
 export default db
--- client/src/pages/BookPage.jsx
+++ client/src/pages/BookPage.jsx
@@ -1,45 +1,45 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, Image, Row, ToggleButton } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import BookApi from "../BookApi";
 import Loading from "../components/Loading";
 import ReactHtmlParser from "react-html-parser"
 import { getBook, setOnBookShelfDB, setOnWishlistDB } from "../firebase/firebasedb";
-import { async } from "@firebase/util";
+import { useAuth } from "../contexts/authContext";
 
 export default function BookPage() {
     const { bookId } = useParams();
+
+    const { currentUser } = useAuth()
 
     const [book, setBook] = useState(null);
     const [onBookshelf, setOnBookshelf] = useState(false);
     const [onWishlist, setOnWishlist] = useState(false);
 
-
     useEffect(() => {
         BookApi.getBook(bookId).then((res) => setBook(res.data));
-        
     }, []);
 
-    useEffect(() => {    
-        bookStatus()
-      });
+    useEffect(() => {
+        updateBookStatus()
+    });
 
-    async function bookStatus(){
-        var bookStatus = await getBook(bookId)
-        if(bookStatus.wishlist) setOnWishlist(true)
-        if(bookStatus.bookShelf)setOnBookshelf(true)
-        console.log(bookStatus)
+    async function updateBookStatus() {
+        if (currentUser) {
+            var bookStatus = await getBook(bookId)
+            if (bookStatus) {
+                setOnWishlist(bookStatus.wishlist === true)
+                setOnBookshelf(bookStatus.bookShelf === true)
+            }
+        }
     }
 
     function toggleBookshelf() {
-        console.log("prije promjene "+ onBookshelf)
         setOnBookShelfDB(bookId, !onBookshelf)
         setOnBookshelf(onBookshelf => !onBookshelf)
-
     }
 
     function toggleWishlist() {
-        console.log("prije promjene "+ onWishlist)
         setOnWishlistDB(bookId, !onWishlist)
         setOnWishlist(onWishlist => !onWishlist)
     }
@@ -56,7 +56,7 @@ export default function BookPage() {
                         {book.subtitle ? book.subtitle + " - " : ""}
                         <i>{book.authors.join(", ")}</i>
                     </h2>
-                    <p>{ReactHtmlParser(book.description)}</p>
+                    <div>{ReactHtmlParser(book.description)}</div>
                     <div className="info-header">Categories</div>
                     <p>{book.categories}</p>
                 </Col>
@@ -71,28 +71,29 @@ export default function BookPage() {
                     <p>{book.publishedDate}</p>
                 </Col>
             </Row>
-            <Row className="g-2 justify-content-center justify-content-md-end">
-                <Col xs="auto">
-                    <ToggleButton
-                        type="checkbox"
-                        variant="outline-primary"
-                        checked={onBookshelf}
-                        onClick={toggleBookshelf}
-                    >
-                        { onBookshelf ? "Remove from bookshelf" : "Add to bookshelf" }
-                    </ToggleButton>
-                </Col>
-                <Col xs="auto">
-                    <ToggleButton
-                        type="checkbox"
-                        variant="outline-primary"
-                        checked={onWishlist}
-                        onClick={toggleWishlist}
-                    >
-                        { onWishlist ? "Remove from wishlist" : "Add to wishlist" }
-                    </ToggleButton>
-                </Col>
-            </Row>
+            {currentUser
+                && <Row className="g-2 justify-content-center justify-content-md-end">
+                    <Col xs="auto">
+                        <ToggleButton
+                            type="checkbox"
+                            variant="outline-primary"
+                            checked={onBookshelf}
+                            onClick={toggleBookshelf}
+                        >
+                            {onBookshelf ? "Remove from bookshelf" : "Add to bookshelf"}
+                        </ToggleButton>
+                    </Col>
+                    <Col xs="auto">
+                        <ToggleButton
+                            type="checkbox"
+                            variant="outline-primary"
+                            checked={onWishlist}
+                            onClick={toggleWishlist}
+                        >
+                            {onWishlist ? "Remove from wishlist" : "Add to wishlist"}
+                        </ToggleButton>
+                    </Col>
+                </Row>}
         </Container>);
 
     else return <Loading />;
